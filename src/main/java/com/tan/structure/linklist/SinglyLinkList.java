565be52bbc369577162edb2470e10fbbd440f1fd
TAN <854918599@qq.com>
2018-10-07 23:06:16 +0800

    1、添加单链表结构； 2、添加单链表翻转、链表环检测方法及测试代码。

---
+++ src/main/java/com/tan/structure/linklist/SinglyLinkList.java
@@ -0,0 +1,101 @@
+package com.tan.structure.linklist;
+
+import java.util.Stack;
+
+public class SinglyLinkList {
+
+    //链表反转
+    public static Node reverse(Node list) {
+        Node currentNode = list;
+        Node reverseHead = null;
+        Node previousNode = null;
+
+        while (currentNode != null) {
+            Node nextNode = currentNode.getNext();
+            if (nextNode == null) {
+                reverseHead = currentNode;
+            }
+            currentNode.setNext(previousNode);
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+        return reverseHead;
+    }
+
+    //用栈链表反转
+    public static Node reverseWithStack(Node head) {
+        if (head == null) {
+            return null;
+        }
+
+        Node cur = head;
+        if (cur.getNext() == null) {
+            return cur;
+        }
+
+        Stack<Node> stack = new Stack<Node>();
+        while (cur.getNext() != null) {
+            stack.push(cur);
+            cur = cur.getNext();
+        }
+        Node rHead = cur;
+        while (!stack.empty()) {
+            cur.setNext(stack.pop());
+            cur = cur.getNext();
+        }
+        cur.setNext(null);
+        return rHead;
+    }
+
+    //检查链表是否有环
+    public static boolean checkCircle(Node head) {
+        if (head == null) {
+            return false;
+        }
+
+        Node slow = head;
+        Node fast = head.getNext();
+        while (fast != null && fast.getNext() != null) {
+            slow = slow.getNext();
+            fast = fast.getNext().getNext();
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static class Node {
+        private int data;
+        private Node next;
+
+        public Node() {
+        }
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public int getData() {
+            return data;
+        }
+
+        public void setData(int data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+}
